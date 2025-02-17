0a26f6521ccfc41f9d8066923235f476b479e688
KarenDC5 <karen.duran@iteso.mx>
2025-02-17 16:48:16 -0600

    primer

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,26 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Dados dos número, determinar si uno es múltiplo de otro
+Karen Durán
+757136 
+Ingeniería Civil
+Algoritmos y Programación 
+ESI232B2
+17/02/25
+10 minutos 
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero_1 = int(input("Introduzca un número: "))
+numero_2 = int(input("Introduzca otro número: "))
 
 # Proceso
-
+if numero_2 != 0 and numero_1 % numero_2 == 0:
+    resultado = True 
+else:
+    resultado = False
 
 # Salidas
-print(salida)
+if numero_2 != 0 and numero_1 % numero_2 == 0:
+    print("El número", numero_1, "es múltiplo del", numero_2)
+else:
+    print("El número", numero_1, "no es múltiplo del", numero_2)
